a0155ca954061d4bfb771e6504b393c43aac688a
KritiJain17 <kritijain1720@gmail.com>
2025-09-09 01:05:47 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -7,11 +7,32 @@ import {
 import "./App.css";
 import { PageOutlet } from "./styles";
 import { HomePage } from "./components/organisms/HomePage";
+import { CheckoutPage } from "./components/organisms/CheckoutPage";
+import { NAV_CONFIG } from "./constants";
 
 function App() {
   return (
     <PageOutlet>
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route
+            path="/*"
+            element={
+              <>
+                <Routes>
+                  <Route
+                    path={NAV_CONFIG?.NAV_CHECKOUT}
+                    element={<CheckoutPage />}
+                  ></Route>
+                </Routes>
+              </>
+            }
+          ></Route>
+        </Routes>
+
+        <HomePage />
+        <CheckoutPage />
+      </Router>
     </PageOutlet>
   );
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,7 @@ export const COSNTANTS = {
   STANDARD: "Standard",
   PREMIUM: "Premium",
 };
+
+export const NAV_CONFIG = {
+  NAV_CHECKOUT: "/checkout",
+};
