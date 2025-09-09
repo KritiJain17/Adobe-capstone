6046f840923fc74c673ee58b888fcadb21cd7d77
KritiJain17 <73176086+KritiJain17@users.noreply.github.com>
2025-09-09 13:06:23 +0530

    Update index.js

--- src/components/organisms/HomePage/index.js
+++ src/components/organisms/HomePage/index.js
@@ -8,21 +8,23 @@ export const HomePage = () => {
       cardHeader: COSNTANTS?.STANDARD,
       cardHeaderColor: "#3B82F6",
       mrp: 2000,
-      gst: 250,
+      gst: 360,
       duration: "9 months",
+      type:COSNTANTS?.STANDARD,
     },
     {
       cardHeader: COSNTANTS?.PREMIUM,
       cardHeaderColor: "#FFC300",
       mrp: 4000,
-      gst: 500,
+      gst: 720,
       duration: "12 months",
+      type:COSNTANTS?.PREMIUM,
     },
   ];
   return (
     <SubscriptionCardMenu>
       {cardDetails?.map((card) => {
-        return <SubscriptionCard card={card}></SubscriptionCard>;
+        return <SubscriptionCard key = {`cardType${card?.cardHeader}`} card={card}></SubscriptionCard>;
       })}
     </SubscriptionCardMenu>
   );
