433f6036a89394b46c30582783d3f6e586ec94fe
KritiJain17 <73176086+KritiJain17@users.noreply.github.com>
2025-09-09 13:05:19 +0530

    Update index.js

--- src/components/organisms/CheckoutPage/index.js
+++ src/components/organisms/CheckoutPage/index.js
@@ -3,13 +3,47 @@ import {
   CheckoutPageContainer,
   CheckoutPageHeader,
   CheckoutBox,
+  CheckoutBoxTile,
+  TileTitle, 
+  TileValue, 
 } from "./styles";
+import { Button } from "../../molecules/Button";
+import { useLocation } from "react-router-dom";
 
 export const CheckoutPage = () => {
+  const location = useLocation();
+let card = location?.state;
+console.log(card)
+  let costs = [
+    {
+      title: "MRP",
+      value: card?.mrp
+    },
+     {
+      title: "GST",
+      value: card?.gst
+    },
+     {
+      title: "TOTAL",
+      value: card?.mrp + card?.gst
+    },
+  ]
   return (
     <CheckoutPageContainer>
-      <CheckoutPageHeader></CheckoutPageHeader>
-      <CheckoutBox></CheckoutBox>
+      
+      <CheckoutBox>
+        <CheckoutPageHeader>Billing Summary</CheckoutPageHeader>
+        {
+          costs?.map((cost) => {
+            return (
+               <CheckoutBoxTile key = {`costType${cost?.title}`}>
+          <TileTitle>{cost?.title}</TileTitle>
+          <TileValue>{cost?.value}</TileValue>
+        </CheckoutBoxTile>
+          )})
+        }
+      </CheckoutBox>
+      <Button margin = "3rem 0 0 0" buttonTitle = "Buy" padding = "0.7rem 2rem"/>
     </CheckoutPageContainer>
   );
 };
