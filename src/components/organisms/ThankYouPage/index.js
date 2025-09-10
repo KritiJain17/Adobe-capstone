d8242b8cc31f79869e456ef5cccc6972bfa7a2d5
KritiJain17 <73176086+KritiJain17@users.noreply.github.com>
2025-09-10 19:21:52 +0530

    Update Thank you page

--- src/components/organisms/ThankYouPage/index.js
+++ src/components/organisms/ThankYouPage/index.js
@@ -1,5 +1,35 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export const ThankYouPage = () => {
-  return <h1>Thank You!</h1>;
+  const location = useLocation();
+let card = location?.state;
+console.log(card);
+  useEffect(() => {
+    if (window.adobe && window.adobe.target) {
+      window.adobe.target.getOffer({
+        mbox: "kj-mbox",
+        params: {
+          subscriptionType: card?.cardHeader, // e.g. "standard" or "premium"
+        },
+        success: function (offers) {
+          window.adobe.target.applyOffer({
+            mbox: "kj-mbox",
+            selector: "#kjtarget",
+            offer: offers,
+          });
+        },
+        error: function (status, error) {
+          console.error("Custom mbox failed:", status, error);
+        },
+      });
+    }
+  }, [card?.cardHeader]);
+  return (
+<>
+<h1>Thank You!</h1>
+  <div id="kjtargetapstone"></div>
+</>
+  );
 };
